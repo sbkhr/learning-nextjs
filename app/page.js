671111697bcebb5172722496d6c13225efab2fb6
Sbkhr <heybukhari@gmail.com>
2024-12-09 22:42:08 -0500

    edited Home

--- app/page.js
+++ app/page.js
@@ -1,5 +1,15 @@
 import Image from "next/image";
 
+export default function Home(){
+  return (
+    <div>
+      <h1>This is Home!</h1>
+    </div>
+  );
+}
+
+// Original Home code
+/*
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
@@ -99,3 +109,4 @@ export default function Home() {
     </div>
   );
 }
+*/
